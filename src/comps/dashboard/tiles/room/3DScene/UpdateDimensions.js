16318d9a5de826bbb1f96dc8c9ef66dd997f137a
Epi-Yeti <WhoIsBigfoot@protonmail.com>
2024-09-26 14:57:12 -0600

    minor 3d room edits

--- src/comps/dashboard/tiles/room/3DScene/UpdateDimensions.js
+++ src/comps/dashboard/tiles/room/3DScene/UpdateDimensions.js
@@ -1,19 +1,53 @@
 import * as THREE from 'three';
 
-export const updateDimensions = (dimensions, clippingPlanes) => {
-  console.log('UpdateDimensions called with:', { dimensions, clippingPlanes });
+export const updateDimensions = (dimensions, clippingPlanes, pivotCorner = 'topLeftFront') => {
+  console.log('UpdateDimensions called with:', { dimensions, clippingPlanes, pivotCorner });
 
   const width = isNaN(dimensions.width) ? 1 : dimensions.width;
   const length = isNaN(dimensions.length) ? 1 : dimensions.length;
   const height = isNaN(dimensions.height) ? 1 : dimensions.height;
 
   if (clippingPlanes && clippingPlanes.length === 6) {
-    clippingPlanes[0].constant = width / 2;
-    clippingPlanes[1].constant = width / 2;
-    clippingPlanes[2].constant = height / 2;
-    clippingPlanes[3].constant = height / 2;
-    clippingPlanes[4].constant = length / 2;
-    clippingPlanes[5].constant = length / 2;
+    // Determine offsets based on pivot corner
+    let xOffset, yOffset, zOffset;
+    switch (pivotCorner) {
+      case 'bottomLeftBack':
+        xOffset = width; yOffset = height; zOffset = length;
+        break;
+      case 'bottomRightBack':
+        xOffset = 0; yOffset = height; zOffset = length;
+        break;
+      case 'topLeftBack':
+        xOffset = width; yOffset = 0; zOffset = length;
+        break;
+      case 'topRightBack':
+        xOffset = 0; yOffset = 0; zOffset = length;
+        break;
+      case 'bottomLeftFront':
+        xOffset = width; yOffset = height; zOffset = 0;
+        break;
+      case 'bottomRightFront':
+        xOffset = 0; yOffset = height; zOffset = 0;
+        break;
+      case 'topLeftFront':
+        xOffset = width; yOffset = 0; zOffset = 0;
+        break;
+      case 'topRightFront':
+        xOffset = 0; yOffset = 0; zOffset = 0;
+        break;
+      default:
+        console.warn('Invalid pivot corner specified, defaulting to bottomLeftBack');
+        xOffset = width; yOffset = height; zOffset = length;
+    }
+
+    // Update clipping planes based on pivot
+    clippingPlanes[0].constant = xOffset;        // Right
+    clippingPlanes[1].constant = width - xOffset; // Left
+    clippingPlanes[2].constant = yOffset;        // Top
+    clippingPlanes[3].constant = height - yOffset; // Bottom
+    clippingPlanes[4].constant = zOffset;        // Front
+    clippingPlanes[5].constant = length - zOffset; // Back
+
     console.log('Updated clipping planes:', clippingPlanes.map(plane => plane.constant));
   } else {
     console.warn('Clipping planes not available or incorrect number');
